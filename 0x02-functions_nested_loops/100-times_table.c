ce379fd549db81e5cfd6009738119f7ca8cbd6f7
MsIdaj <manjaraidah@gmail.com>
2022-07-05 18:37:47 +0000

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,51 +1,46 @@
 #include "main.h"
-
 /**
- * print_times_table - prints the times table up to the integer input
- * @n: integer input
+ * print_times_table - print times table start with 0
+ * @n: the n times
  *
  * Return: void
  */
-
 void print_times_table(int n)
 {
-	int a, b, c, d, e, f;
-
-	if (n >= 0 && n < 15)
+	if (n > 0 && n < 15)
 	{
-		for (a = 0; a <=  n; a++)
+		int i;
+		int j;
+
+		for (i = 0; i <= n; i++)
 		{
-			for (b = 0; b <= n; b++)
+			for (j = 0; j <= n; j++)
 			{
-				c = a * b; d = c / 100; e = (c /  10) % 10;
-				f = (c % 100) % 10;
-				if (b == 0)
-				{
-					_putchar('0');
-				}
-				else if (c < 10)
-				{
-					_putchar(' '); _putchar(' ');
-					_putchar('0' + f);
-				}
-				else if (c < 100)
+				int val = i * j;
+
+				if (val >= 100)
 				{
 					_putchar(' ');
-					_putchar('0' + e); _putchar('0' + f);
+					_putchar((val / 100) + '0');
+					_putchar(((val / 10) % 10) + '0');
 				}
-				else
+				else if (val >= 10)
 				{
-					_putchar('0' + d);
-					_putchar('0' + e);
-					_putchar('0' + f);
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((val / 10) + '0');
 				}
-				if (b < n)
+				else if (val < 10 && j > 0)
 				{
-					_putchar(','); _putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
 				}
-				else
-					_putchar('\n');
+				_putchar((val % 10) + '0');
+				if (j < n)
+					_putchar(',');
 			}
+			_putchar('\n');
 		}
 	}
 }
