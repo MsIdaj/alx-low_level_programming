c2cace5f5111e81646b375f8505031d6a99559f9
MsIdaj <manjaraidah@gmail.com>
2022-07-07 12:56:11 +0000

    Update

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -10,7 +11,7 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-int _putchar(char c);
+int _putchar(char);
 void print_numbers(void);
 void print_number(int n);
 
